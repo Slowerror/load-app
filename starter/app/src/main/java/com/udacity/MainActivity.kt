2e180b2e14f0dbc953f1482c1412811ccef74d5c
Nikolay Zetkin <nszetkin76@gmail.com>
2022-10-18 15:04:59 +0300

    добавление проекта

--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -11,11 +11,13 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
+import com.udacity.databinding.ActivityMainBinding
+
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private var downloadID: Long = 0
 
@@ -25,14 +27,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        /*binding.root.findViewById<>(R.id.custom_button)
         custom_button.setOnClickListener {
             download()
-        }
+        }*/
     }
 
     private val receiver = object : BroadcastReceiver() {
