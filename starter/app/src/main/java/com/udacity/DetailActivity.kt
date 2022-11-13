a836a08280eb6b0e4f1e55848aa004e0ad5e7aaa
Nikolay Zetkin <nszetkin76@gmail.com>
2022-11-13 05:28:21 +0300

    Добавил макет DetailActivity.kt

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,7 +1,10 @@
 package com.udacity
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.udacity.MainActivity.Companion.KEY_FILE_NAME
+import com.udacity.MainActivity.Companion.KEY_STATUS
 import com.udacity.databinding.ActivityDetailBinding
 
 
@@ -15,6 +18,20 @@ class DetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-    }
 
+        val fileName = intent.getStringExtra(KEY_FILE_NAME)
+        val status = intent.getStringExtra(KEY_STATUS)
+
+        binding.layoutContentDetail.nameFileDescription.text = fileName
+        if (status == getString(R.string.success_download_status)) {
+            binding.layoutContentDetail.statusDescription.setTextColor(Color.GREEN)
+        } else {
+            binding.layoutContentDetail.statusDescription.setTextColor(Color.RED)
+        }
+        binding.layoutContentDetail.statusDescription.text = status
+
+        binding.layoutContentDetail.goBackButton.setOnClickListener {
+            finish()
+        }
+    }
 }
