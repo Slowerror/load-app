b66a8ec82cd34cf3650c4e68c38dda6d34065c42
Nikolay Zetkin <nszetkin76@gmail.com>
2022-11-13 03:32:22 +0300

    Предварительно сделал LoadingButton.kt и NotificationManager

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,45 +2,191 @@ package com.udacity
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
+import android.text.TextPaint
 import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
 
-    private val valueAnimator = ValueAnimator()
+    private var backgroundColorButton by Delegates.notNull<Int>()
+    private var backgroundColorProgress by Delegates.notNull<Int>()
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    private var buttonProgress = 0f
+    private lateinit var textDownloadButton: String
+    private lateinit var textLoadingButton: String
+    private val textBounds = Rect()
+    private var valueAnimator = ValueAnimator.ofFloat(0f, 1f)
 
+    private var buttonState: ButtonState by Delegates.observable(ButtonState.Completed) { _, _, new ->
+        when (new) {
+            ButtonState.Loading -> {
+                showAnimation()
+            }
+            ButtonState.Completed -> {
+                stopAnimation()
+            }
+            ButtonState.Clicked -> {
+                isEnabled = true
+                return@observable
+            }
+        }
     }
 
+    private val rect = RectF(0f, 0f, 0f, 0f)
+
+    private lateinit var paintBackground: Paint
+    private lateinit var textPaint: TextPaint
+    private lateinit var paintProgressCircle: Paint
 
     init {
 
+        if (attrs != null) {
+            initAttributes(attrs)
+        }
+
+        initPaint()
     }
 
+    private fun initAttributes(attrs: AttributeSet?) {
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            backgroundColorButton = getColor(R.styleable.LoadingButton_downloadColorButton, Color.GRAY)
+            backgroundColorProgress = getColor(R.styleable.LoadingButton_loadingColorButton, Color.DKGRAY)
+            textDownloadButton = getString(R.styleable.LoadingButton_downloadTextButton).toString()
+            textLoadingButton = getString(R.styleable.LoadingButton_loadingTextButton).toString()
+        }
+    }
 
-    override fun onDraw(canvas: Canvas?) {
+    private fun initPaint() {
+        paintBackground = Paint((Paint.ANTI_ALIAS_FLAG)).apply {
+            color = backgroundColorButton
+            style = Paint.Style.FILL
+        }
+
+        textPaint = TextPaint((Paint.ANTI_ALIAS_FLAG)).apply {
+            color = Color.WHITE
+            textAlign = Paint.Align.CENTER
+            textSize = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_SP,
+                20f,
+                resources.displayMetrics
+            )
+            typeface = Typeface.DEFAULT_BOLD
+        }
+
+        paintProgressCircle = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = Color.WHITE
+            style = Paint.Style.FILL
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        drawBackgroundProgress(canvas)
+
+        when (buttonState) {
+            ButtonState.Loading -> {
+                drawBackgroundProgress(canvas, buttonProgress)
+
+                textPaint.getTextBounds(textLoadingButton, 0, textLoadingButton.length, textBounds)
+                val start = (widthSize / 2 + textBounds.exactCenterX()) + textBounds.height() / 2
+
+                canvas.drawArc(
+                    start,
+                    (heightSize / 2 - textBounds.height() / 2).toFloat(),
+                    start + textBounds.height(),
+                    (heightSize / 2 + textBounds.height() / 2).toFloat(),
+                    0F,
+                    buttonProgress * 360,
+                    true,
+                    paintProgressCircle
+                )
+
+                drawText(canvas, textLoadingButton)
+            }
+            else -> {
+                drawText(canvas, textDownloadButton)
+            }
+        }
+        /*if (buttonState == ButtonState.Loading) {
+
+
+        } else {
+
+        }*/
+
     }
+
+    /*override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        widthSize = w
+        heightSize = h
+        rect.set(0f, 0f, widthSize.toFloat(), heightSize.toFloat())
+
+    }*/
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
         val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
-        val h: Int = resolveSizeAndState(
-            MeasureSpec.getSize(w),
-            heightMeasureSpec,
-            0
-        )
+        val h: Int = resolveSizeAndState(MeasureSpec.getSize(w), heightMeasureSpec, 0)
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
     }
 
+    private fun drawBackgroundProgress(canvas: Canvas, progress: Float? = null) {
+        if (progress != null) {
+            paintBackground.alpha = 200
+        }
+        canvas.drawRect(0f, 0f,
+            widthSize.toFloat() * (progress ?: 1f),
+            heightSize.toFloat(),
+            paintBackground
+        )
+
+    }
+
+    private fun drawText(canvas: Canvas, text: String) {
+        val xPos = widthSize / 2f
+        val yPos = heightSize / 2f - (textPaint.descent() + textPaint.ascent()) / 2
+        canvas.drawText(text, xPos, yPos, textPaint)
+    }
+
+    private fun showAnimation() {
+        valueAnimator = ValueAnimator.ofFloat(0f, 1f).apply {
+            addUpdateListener {
+                buttonProgress = animatedValue as Float
+                invalidate()
+            }
+            repeatCount = ValueAnimator.INFINITE
+            repeatMode = ValueAnimator.RESTART
+            duration = 3000
+            start()
+        }
+        isEnabled = false
+
+    }
+
+    private fun stopAnimation() {
+        valueAnimator.cancel()
+        invalidate()
+        isEnabled = true
+        buttonState = ButtonState.Clicked
+    }
+
+    fun setupButtonState(state: ButtonState) {
+        buttonState = state
+    }
 }
--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.udacity
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
@@ -8,7 +9,10 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import com.udacity.databinding.ActivityMainBinding
@@ -25,6 +29,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    private lateinit var url: String
+    private lateinit var fileName: String
+    private lateinit var status: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -33,36 +41,147 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
-        /*binding.root.findViewById<>(R.id.custom_button)
-        custom_button.setOnClickListener {
-            download()
-        }*/
+        getUrl()
+        createChanel()
+
+        binding.layoutContentMain.customButton.setOnClickListener {
+            if (::url.isInitialized) {
+                binding.layoutContentMain.customButton.setupButtonState(ButtonState.Loading)
+                download()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    R.string.toast_message_select_file,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+    }
+
+    private fun getUrl() {
+        binding.layoutContentMain.downloadList.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.downloadFromGlide -> {
+                    url = GLIDE_URL
+                    fileName = getString(R.string.glide_description_download)
+                }
+                R.id.downloadFromRepository -> {
+                    url = REPOSITORY_URL
+                    fileName = getString(R.string.repository_description_download)
+                }
+                R.id.downloadFromRetrofit -> {
+                    url = RETROFIT_URL
+                    fileName = getString(R.string.retrofit_description_download)
+                }
+            }
+        }
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            id?.let {
+                val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+                val query = DownloadManager.Query()
+                query.setFilterById(it)
+                val cursor = downloadManager.query(query)
+                if (cursor.moveToFirst()) {
+                    val index = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
+                    status = if (DownloadManager.STATUS_SUCCESSFUL == cursor.getInt(index)) {
+                        getString(R.string.success_download_status)
+                    } else {
+                        getString(R.string.fail_download_status)
+                    }
+                    sendNotification()
+                    binding.layoutContentMain.customButton.setupButtonState(ButtonState.Completed)
+                }
+            }
+
         }
     }
 
     private fun download() {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
+                .setDescription(fileName)
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        downloadID = downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+    }
+
+
+    private fun sendNotification() {
+        val contentIntent = Intent(applicationContext, DetailActivity::class.java).apply {
+            putExtra(KEY_FILE_NAME, fileName)
+            putExtra(KEY_STATUS, status)
+        }
+
+        pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            CHANNEL_ID
+        ).apply {
+            setSmallIcon(R.drawable.ic_assistant_black_24dp)
+            setContentTitle(getString(R.string.notification_title))
+            setContentText(getString(R.string.notification_description))
+            setContentIntent(pendingIntent)
+            setAutoCancel(true)
+            addAction(
+                R.drawable.ic_baseline_cloud_done_24,
+                getString(R.string.notification_button),
+                pendingIntent
+            )
+            priority = NotificationManager.IMPORTANCE_DEFAULT
+        }.build()
+
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NOTIFICATION_ID, builder)
+    }
+
+    private fun createChanel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChanel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                enableLights(true)
+                enableVibration(false)
+                description = getString(R.string.channel_description)
+            }
+
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChanel)
+
+        }
     }
 
     companion object {
+        private const val GLIDE_URL = "https://github.com/bumptech/glide"
+        private const val RETROFIT_URL = "https://github.com/square/retrofit"
+        private const val REPOSITORY_URL =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter"
+
         private const val URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+
+        private const val NOTIFICATION_ID = 0
         private const val CHANNEL_ID = "channelId"
+        private const val CHANNEL_NAME = "Download notification"
+
+        private const val KEY_FILE_NAME = "key file"
+        private const val KEY_STATUS = "key status"
     }
 
 }
