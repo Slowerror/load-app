21cf1d41b89aea4b0b32bd495e6e02aba2413521
Nikolay Zetkin <nszetkin76@gmail.com>
2022-11-13 15:01:29 +0300

    Перенес константы в объект Constants.kt, перенес реализацию уведомлений в файл NotificationUtils.kt

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,10 +1,12 @@
 package com.udacity
 
+import android.app.NotificationManager
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.udacity.MainActivity.Companion.KEY_FILE_NAME
-import com.udacity.MainActivity.Companion.KEY_STATUS
+import com.udacity.Constants.KEY_FILE_NAME
+import com.udacity.Constants.KEY_STATUS
 import com.udacity.databinding.ActivityDetailBinding
 
 
@@ -19,9 +21,22 @@ class DetailActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+
         val fileName = intent.getStringExtra(KEY_FILE_NAME)
         val status = intent.getStringExtra(KEY_STATUS)
 
+        setupValues(fileName, status)
+
+        binding.layoutContentDetail.goBackButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancelNotification()
+    }
+
+    private fun setupValues(fileName: String?, status: String?) {
         binding.layoutContentDetail.nameFileDescription.text = fileName
         if (status == getString(R.string.success_download_status)) {
             binding.layoutContentDetail.statusDescription.setTextColor(Color.GREEN)
@@ -29,9 +44,7 @@ class DetailActivity : AppCompatActivity() {
             binding.layoutContentDetail.statusDescription.setTextColor(Color.RED)
         }
         binding.layoutContentDetail.statusDescription.text = status
-
-        binding.layoutContentDetail.goBackButton.setOnClickListener {
-            finish()
-        }
     }
+
+
 }
